3991c7dce62259861a82926c664dca2026943414
Valentin Fazenda <valentin.fazenda@hotmail.com>
2025-06-03 17:25:37 +0200

    feat: Add AWS Lambda function update and invocation to deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -5,6 +5,7 @@ $accountId = '654654340294'
 $region    = 'eu-north-1'
 $registry  = "$accountId.dkr.ecr.$region.amazonaws.com"
 $repoUri   = "$registry/$image"
+$imageUri = "$repoUri" + ":latest"
 
 aws ecr get-login-password --region $region |
     docker login --username AWS --password-stdin $registry
@@ -15,3 +16,13 @@ if ($LASTEXITCODE) { exit 1 }
 
 docker push "$repoUri"
 if ($LASTEXITCODE) { exit 1 }
+
+aws lambda update-function-code --function-name "$functionName" --image-uri "$imageUri" --region "$region"
+if ($LASTEXITCODE) { exit 1 }
+
+# Invoke Lambda function
+$outputFile = "output.json"
+aws lambda invoke --function-name $functionName --region $region --payload '{}' $outputFile --cli-binary-format raw-in-base64-out
+if ($LASTEXITCODE) { exit 1 }
+
+Get-Content $outputFile
