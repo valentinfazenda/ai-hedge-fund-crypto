71cd52e6dcef3d50ab314699fd9e4d9aa3c23e8a
Valentin Fazenda <valentin.fazenda@hotmail.com>
2025-06-12 18:37:01 +0200

    refactor: Improve logger setup by ensuring LOG_LEVEL is always set and enhancing log level output

--- app/src/utils/logger.py
+++ app/src/utils/logger.py
@@ -1,8 +1,12 @@
 import os
 import logging
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def setup_logger():
-    log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+    log_level = os.getenv("LOG_LEVEL").upper()
+    print(f"Setting log level to: {log_level}")
     numeric_level = getattr(logging, log_level, logging.INFO)
     logging.basicConfig(
         level=numeric_level,
