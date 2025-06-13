ce50526cbfdc8678faf05bdbdcdddfac14472ca0
Valentin Fazenda <valentin.fazenda@hotmail.com>
2025-06-13 10:45:25 +0200

    refactor: Remove debug print statement from logger setup

--- app/src/utils/logger.py
+++ app/src/utils/logger.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 def setup_logger():
     log_level = os.getenv("LOG_LEVEL").upper()
-    print(f"Setting log level to: {log_level}")
     numeric_level = getattr(logging, log_level, logging.INFO)
     logging.basicConfig(
         level=numeric_level,
